c977f96dc681e8f6796b10aacf82e4e2610a0f1b
msashraf3 <mshamimas@proton.me>
2024-08-24 23:34:49 +0600

    trying the button. and making the sit work

---
+++ js/index.js
@@ -0,0 +1,13 @@
+document.getElementById('A1').addEventListener('click', function (){
+    if (this.classList.contains('green-color')) {
+        const gettingText = document.getElementById('A1');
+    gettingText.classList.remove('green-color');
+    gettingText.classList.add('text-[#03071250]');
+    } else {
+        const gettingText = document.getElementById('A1');
+    gettingText.classList.add('green-color');
+    gettingText.classList.remove('text-[#03071250]');
+    }
+    
+    // console.log('hello')
+})
